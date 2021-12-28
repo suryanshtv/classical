c3b843710600febcccb01f0a1d48ab275fdea406
Suryansh <suryanshsingh67@gmail.com>
2021-12-28 14:05:05 +0530

    added emojis ✨ 😉

--- src/mapping/playerCanvas.js
+++ src/mapping/playerCanvas.js
@@ -11,6 +11,8 @@ module.exports = {
         let songtitle = songtitlemain.slice(0, 28)
         let songtitle1 = songtitlemain.slice(28, 66)+("...")
         // Create the canvas
+        registerFont('./src/mapping/fonts/NotoColorEmoji.ttf', { family: 'Emoji' });
+        registerFont('./src/mapping/fonts/SamsungColorEmoji.ttf', { family: 'Emoji' });
         registerFont('./src/mapping/fonts/NotoSans-Bold.ttf', { family: 'Noto' });
         registerFont('./src/mapping/fonts/NotoSansJP-Bold.otf', { family: 'Noto' });
         registerFont('./src/mapping/fonts/Poppins-Bold.ttf', { family: 'Poppins' });
@@ -26,11 +28,11 @@ module.exports = {
             ctx.drawImage(thumb, 40, 40, 363, 226);
         }
         ctx.drawImage(cover, 419, 44, 360, 215);
-        ctx.font = '26px "Rubik", "Poppins", "Noto"';
+        ctx.font = '26px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillStyle = '#ffffff';
         ctx.fillText(songtitle, 432, 85);
         ctx.fillText(songtitle1, 432, 125);
-        ctx.font = '26px "Rubik", "Poppins", "Noto"';
+        ctx.font = '26px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillText(``+artist, 432, 205);
         ctx.fillText(``+durationMS, 432, 235);
         ctx.drawImage(base, 0, 0, canvas.width, canvas.height);
@@ -38,6 +40,8 @@ module.exports = {
     },
     async spotifyPlaylist(title, length, author) {
         //fonts
+        registerFont('./src/mapping/fonts/NotoColorEmoji.ttf', { family: 'Emoji' });
+        registerFont('./src/mapping/fonts/SamsungColorEmoji.ttf', { family: 'Emoji' });
         registerFont('./src/mapping/fonts/NotoSans-Bold.ttf', { family: 'Noto' });
         registerFont('./src/mapping/fonts/NotoSansJP-Bold.otf', { family: 'Noto' });
         registerFont('./src/mapping/fonts/Poppins-Bold.ttf', { family: 'Poppins' });
@@ -49,10 +53,10 @@ module.exports = {
         ctx.drawImage(base, 482, 2, 483, 295);
         ctx.fillStyle = '#b7ff70';
         ctx.fillText(`Spotify`, 495, 80);
-        ctx.font = '26px "Rubik", "Poppins", "Noto"';
+        ctx.font = '26px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillStyle = '#ffffff';
         ctx.fillText(`Added `+ title +` to the queue.`, 495, 48);
-        ctx.font = '22px "Rubik", "Poppins", "Noto"';
+        ctx.font = '22px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillText(`tracks: `+ length, 492, 124);
         ctx.fillText(`loaded.`, 680, 124);
         ctx.fillText(`Requested by `+ author.username, 495, 276)
@@ -62,6 +66,8 @@ module.exports = {
     },
     async spotifyTrack(searched, author) {
         //fonts
+        registerFont('./src/mapping/fonts/NotoColorEmoji.ttf', { family: 'Emoji' });
+        registerFont('./src/mapping/fonts/SamsungColorEmoji.ttf', { family: 'Emoji' });
         registerFont('./src/mapping/fonts/NotoSans-Bold.ttf', { family: 'Noto' });
         registerFont('./src/mapping/fonts/NotoSansJP-Bold.otf', { family: 'Noto' });
         registerFont('./src/mapping/fonts/Poppins-Bold.ttf', { family: 'Poppins' });
@@ -83,24 +89,26 @@ module.exports = {
         ctx.drawImage(thumbnail, 16, -100, 460, 460)
         const base = await loadImage('./src/mapping/images/backCover.png');
         ctx.drawImage(base, 482, 2, 483, 295);
-        ctx.font = '26px "Rubik", "Poppins", "Noto"';
+        ctx.font = '26px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillStyle = '#ffffff';
         ctx.fillText(`Added `+ song.title + ` to the queue.`, 495, 48);
         ctx.fillStyle = '#b7ff70';
         ctx.fillText(`Spotify`, 495, 80);
         ctx.fillStyle = '#ffffff';
-        ctx.font = '22px "Rubik", "Poppins", "Noto"';
+        ctx.font = '22px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillText(`By `+ song.channel, 492, 124);
         ctx.fillText(``, 680, 124);
         ctx.fillText(`Requested by `+ author.username, 495, 274)
         const overlay = await loadImage('./src/mapping/images/spotifyTrack.png');
         ctx.drawImage(overlay, 0, 0, canvas.width, canvas.height);
-        ctx.font = '23px "Rubik", "Poppins", "Noto"';
+        ctx.font = '23px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillText(`${(await convertTime(song.duration))}`, 395, 281);
         return canvas;
     },
     async youtubePlaylist(searched, author) {
         //fonts
+        registerFont('./src/mapping/fonts/NotoColorEmoji.ttf', { family: 'Emoji' });
+        registerFont('./src/mapping/fonts/SamsungColorEmoji.ttf', { family: 'Emoji' });
         registerFont('./src/mapping/fonts/NotoSans-Bold.ttf', { family: 'Noto' });
         registerFont('./src/mapping/fonts/NotoSansJP-Bold.otf', { family: 'Noto' });
         registerFont('./src/mapping/fonts/Poppins-Bold.ttf', { family: 'Poppins' });
@@ -114,12 +122,12 @@ module.exports = {
         ctx.drawImage(base, 482, 2, 483, 295);
         const thumbnail = await loadImage(searched?.tracks[0].displayThumbnail("maxresdefault"));
         ctx.drawImage(thumbnail, 16, 13, 458, 272)
-        ctx.font = '26px "Rubik", "Poppins", "Noto"';
+        ctx.font = '26px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillStyle = '#ff3247';
         ctx.fillText(`YouTube`, 495, 80);
         ctx.fillStyle = '#ffffff';
         ctx.fillText(`Added `+ playlistTitle +` to the queue.`, 495, 48);
-        ctx.font = '22px "Rubik", "Poppins", "Noto"';
+        ctx.font = '22px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillText(`tracks: `+ searched.tracks.length, 492, 124);
         ctx.fillText(`loaded.`, 680, 124);
         ctx.fillText(`Requested by `+ author.username, 495, 276)
@@ -129,6 +137,8 @@ module.exports = {
     },
     async youtubeTrack(song, author) {
         //fonts
+        registerFont('./src/mapping/fonts/NotoColorEmoji.ttf', { family: 'Emoji' });
+        registerFont('./src/mapping/fonts/SamsungColorEmoji.ttf', { family: 'Emoji' });
         registerFont('./src/mapping/fonts/NotoSans-Bold.ttf', {family: 'Noto'});
         registerFont('./src/mapping/fonts/NotoSansJP-Bold.otf', {family: 'Noto'});
         registerFont('./src/mapping/fonts/Poppins-Bold.ttf', {family: 'Poppins'});
@@ -152,17 +162,17 @@ module.exports = {
         ctx.drawImage(thumbnail, 16, 13, 458, 272)
         ctx.drawImage(ytTrackBase, 482, 2, 483, 295)
         ctx.drawImage(channelIcon, 482, 137, 65, 65)
-        ctx.font = '26px "Rubik", "Poppins", "Noto"'
+        ctx.font = '26px "Rubik", "Poppins", "Noto", "Emoji"'
         ctx.fillStyle = '#ffffff'
         ctx.fillText(songtitle, 495, 48)
         ctx.fillText(songtitle1, 495, 80)
-        ctx.font = '22px "Rubik", "Poppins", "Noto"'
+        ctx.font = '22px "Rubik", "Poppins", "Noto", "Emoji"'
         ctx.fillText(views, 492, 124)
         ctx.fillText(song.uploadedAt, 680, 124)
         ctx.fillText(channelName, 563, 174);
         ctx.fillText(`Requested by ` + author.username, 495, 276);
         ctx.drawImage(ytTrackOverlay, 0, 0, canvas.width, canvas.height);
-        ctx.font = '23px "Rubik", "Poppins", "Noto"';
+        ctx.font = '23px "Rubik", "Poppins", "Noto", "Emoji"';
         ctx.fillText(`${song.duration}`, 395, 281);
         return canvas;
     },
