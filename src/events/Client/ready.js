c4b01d67c8754ae9cc14ba88099d5420dfc01f27
Suryansh <74529332+ItsSuryansh@users.noreply.github.com>
2021-12-28 11:24:22 +0530

    Update ready.js

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -6,7 +6,7 @@ module.exports = async (client) => {
     client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
 
     //Game
-    let statuses = [`Please do not use this bot for now, we are upgrading some stuffs.`];
+    let statuses = [`v2022.1 | -help`];
     setInterval(function() {
   		let status = statuses[Math.floor(Math.random()*statuses.length)];
   		client.user.setActivity(status, {type: "PLAYING"});
