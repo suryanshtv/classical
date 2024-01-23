b0c76eedf7c2b4fd34b6cbe4ab4c3939eb18aaeb
Suryansh Singh <74529332+suryanshtv@users.noreply.github.com>
2024-01-23 23:02:01 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,19 @@
 module.exports = {
     token: process.env.TOKEN || "",  // your bot token
     prefix: process.env.PREFIX || "--", // bot prefix
-    ownerID: process.env.OWNERID || "690484458209542224", //your discord id
-    SpotifyID: process.env.SPOTIFYID || "da8e3b26c42a499e854d63af02c067e8", // spotify client id
-    SpotifySecret: process.env.SPOTIFYSECRET || "852c85311add4fcbbf23108f0ed0b5bd", // spotify client secret
-    db: process.env.MONGO_URI || "mongodb+srv://Classical-DEV:adminrestapi@cluster0.y86iq.mongodb.net/test-classychan?retryWrites=true&w=majority", // MongoDb URL
-    embedColor: process.env.COlOR || "#e592d8", // embed colour
-    logs: process.env.LOGS || "915163877484933140", // channel id for guild create and delete logs
-    version: process.env.VERSION || "2021.end", // bot version
+    ownerID: process.env.OWNERID || "", //your discord id
+    SpotifyID: process.env.SPOTIFYID || "", // spotify client id
+    SpotifySecret: process.env.SPOTIFYSECRET || "", // spotify client secret
+    db: process.env.MONGO_URI || "", // MongoDb URL
+    embedColor: process.env.COlOR || "", // embed colour
+    logs: process.env.LOGS || "", // channel id for guild create and delete logs
+    version: process.env.VERSION || "", // bot version
 
   nodes: {
 
       host: "0.0.0.0",
       port: 2333,
-      password: "VPS_lavaNode",
+      password: "",
       id: "1",
       retryDelay: 3000,
       secure: false
